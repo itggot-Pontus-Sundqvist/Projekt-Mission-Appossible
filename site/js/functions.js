85a90f18160f075530e6d55b1b96c49d1a8e0b67
itggot-Pontus-Sundqvist <pontus.sundqvist@itggot.se>
2017-04-04 15:16:02 +0200

    Dela upp mobil och desktop separat

--- site/js/functions.js
+++ site/js/functions.js
@@ -2,20 +2,18 @@ function pressed(navi){
   navi.classList.remove("resting");
   navi.classList.add("pressed");
   setTimeout(function(){navi.classList.remove("pressed");navi.classList.add("resting")},1000)
-
-
 }
 
-function slide(){
+function show_drawer(){
   box = document.getElementById("drawer");
   if (box.style.left != "0px"){
     box.style.left = "0px"
   } else {
-    box.style.left = "-350px"
+    box.style.left = "-1000px"
   }
 }
 
 function hide_drawer(){
   box = document.getElementById("drawer");
-  box.style.left = "-350px"
+  box.style.left = "-1000px"
 }
