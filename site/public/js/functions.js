25c961592757e39c3ef4c076315b2848ffcefd1f
itggot-Pontus-Sundqvist <pontus.sundqvist@itggot.se>
2017-04-04 14:09:26 +0200

    Javascript animationer

--- site/public/js/functions.js
+++ site/public/js/functions.js
@@ -1,4 +1,21 @@
 function pressed(navi){
   navi.classList.remove("resting");
   navi.classList.add("pressed");
+  setTimeout(function(){navi.classList.remove("pressed");navi.classList.add("resting")},1000)
+
+
+}
+
+function slide(){
+  box = document.getElementById("drawer");
+  if (box.style.left != "0px"){
+    box.style.left = "0px"
+  } else {
+    box.style.left = "-350px"
+  }
+}
+
+function hide_drawer(){
+  box = document.getElementById("drawer");
+  box.style.left = "-350px"
 }
