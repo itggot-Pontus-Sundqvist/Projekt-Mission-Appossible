21cc173c1e6a9162ab78d4bc20a29e2c3c6613da
itggot-Pontus-Sundqvist <pontus.sundqvist@itggot.se>
2017-04-20 20:45:46 +0200

    Implementera dropdown meny

--- site/public/js/functions.js
+++ site/public/js/functions.js
@@ -10,8 +10,24 @@ function show_drawer(){
   box.classList.remove("hidden_drawer");
 }
 
-function hide_drawer(){
+function show_dropdown(){
+  box = document.getElementById("list");
+  box.classList.add("expanded_list");
+  box.classList.remove("hidden_list");
+}
+
+function change_beverage(name){
+  document.getElementById("beverage").innerHTML = name.id
+  box = document.getElementById("list");
+  box.classList.add("hidden_list");
+  box.classList.remove("expanded_list");
+}
+
+function hide_everything(){
   box = document.getElementById("drawer");
   box.classList.add("hidden_drawer");
   box.classList.remove("expanded_drawer");
+  box = document.getElementById("list");
+  box.classList.add("hidden_list");
+  box.classList.remove("expanded_list");
 }
